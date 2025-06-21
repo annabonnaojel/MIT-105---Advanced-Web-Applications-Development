726abab189bdc740f56592a3d63dc89c4f12ffc5
annabonnaojel <ojelannabonna@gmail.com>
2025-06-21 16:14:01 +0800

    Delete TrainerRegistry_root directory

--- TrainerRegistry_root/TrainerRegistry_site/celery.py
+++
@@ -1,9 +0,0 @@
-# TrainerRegistry/celery.py
-import os
-from celery import Celery
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TrainerRegistry.settings')
-
-app = Celery('TrainerRegistry')
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
